3fa390d2ee6634bf0f156a0511247e832eed3965
7-GB <ibrhemalbasow@gmail.com>
2026-02-23 17:15:33 +0300

    1.1v

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 android {
     namespace = "com.example.firebaseetasks"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "29.0.14206865"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
